cbf678bc7feb383fbfbb54869910bdecd6f48f8c
RV <rafal.piwnicki@gmail.com>
2018-12-05 22:18:49 +0100

    rejestracja gut

--- konta/views.py
+++ konta/views.py
@@ -1,19 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
 # Create your views here.
 def login(request):
-    if request.method == 'POST':
-        if request.POST['haslo1'] == request.POST['haslo2']:]
-            User.objects.get(login=request.POST['login'])
-    else:
-        return render(request, 'konta/login.html')
-
-
+    return render(request, 'konta/login.html')
 
 def rejestracja(request):
-    return render(request, 'konta/rejestracja.html')
+        if request.method == 'POST':
+            if request.POST['haslo1'] == request.POST['haslo2']:
+                try:
+                    user = User.objects.get(username=request.POST['username'])
+                    return render(request, 'konta/rejestracja.html', {'error':'User zajety'})
+                except User.DoesNotExist:
+                    user = User.objects.create_user(request.POST['username'], password=request.POST['haslo1'])
+                    auth.login(request,user)
+                    return redirect ('home')
+            else:
+                return render(request, 'konta/rejestracja.html', {'error':'ZLE HASLO DRUGIE ABO PIERSZE EE'})
+        else:
+            return render(request, 'konta/rejestracja.html')
 
 def wylogowanie(request):
     return render(request, 'konta/zarejestruj.html')
